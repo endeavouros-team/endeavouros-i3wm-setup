28f426f7cd964c99b1d8979a23b3e654e3f27b1f
Johannes Kamprad <archlinux@kamprad.net>
2021-03-13 01:10:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,9 @@ cd endeavouros-i3wm-setup
 cp -R .config /home/$NEW_USER/                                               
 chmod -R +x /home/$NEW_USER/.config/i3/scripts
 cp .gtkrc-2.0 /home/$NEW_USER/
+cp .nanorc /home/$NEW_USER/
 chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.config
 chown $NEW_USER:$NEW_USER /home/$NEW_USER/.gtkrc-2.0
+chown $NEW_USER:$NEW_USER /home/$NEW_USER/.nanorc
 cd ..
 rm -rf endeavouros-i3wm-setup
