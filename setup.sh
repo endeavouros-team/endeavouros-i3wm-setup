14f77621527d6a29a93f72e43dbfea07d5e428c1
Johannes Kamprad <archlinux@kamprad.net>
2021-09-01 02:59:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-
-if [ -f /tmp/new_username.txt ]
-then
-    NEW_USER=$(cat /tmp/new_username.txt)
-else
-    NEW_USER=$(cat /tmp/$chroot_path/etc/passwd | grep "/home" |cut -d: -f1 |head -1)
-fi
-
 git clone https://github.com/endeavouros-team/endeavouros-i3wm-setup.git
 cd endeavouros-i3wm-setup
-cp -R .config /home/$NEW_USER/                                               
-chmod -R +x /home/$NEW_USER/.config/i3/scripts
-cp .gtkrc-2.0 /home/$NEW_USER/
-cp .nanorc /home/$NEW_USER/
-chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.config
-chown $NEW_USER:$NEW_USER /home/$NEW_USER/.gtkrc-2.0
-chown $NEW_USER:$NEW_USER /home/$NEW_USER/.nanorc
+cp -R .config ~/                                            
+chmod -R +x ~/.config/i3/scripts
+cp .gtkrc-2.0 ~/
+cp .nanorc ~/
+dbus-launch dconf load / < ~/xed.dconf
+sed -i 's|\(exec --no-startup-id ~/set_once.sh\)|# \1|' ~/.config/i3/config
 cd ..
 rm -rf endeavouros-i3wm-setup
