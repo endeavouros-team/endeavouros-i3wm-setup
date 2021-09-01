a145055d4403b8f66301e63d6f6a9e4787bbb0f7
Johannes Kamprad <archlinux@kamprad.net>
2021-09-01 03:01:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ cp .gtkrc-2.0 ~/
 cp .nanorc ~/
 dbus-launch dconf load / < ~/xed.dconf
 sed -i 's|\(exec --no-startup-id ~/set_once.sh\)|# \1|' ~/.config/i3/config
+sudo pacman -S --needed - < packages-repository.txt
 cd ..
 rm -rf endeavouros-i3wm-setup
