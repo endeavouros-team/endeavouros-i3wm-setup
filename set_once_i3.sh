e677765942f4b23be629f283437e209d508de6ed
Johannes Kamprad <archlinux@kamprad.net>
2022-05-05 20:02:22 +0200

    Update set_once_i3.sh
    add firewalld applet to not not starting on i3

--- set_once_i3.sh
+++ set_once_i3.sh
@@ -2,5 +2,7 @@
 dbus-launch dconf load / < ~/xed.dconf
 chmod -R +x ~/.config/i3/scripts
 sed -i 's|\(exec --no-startup-id ~/set_once_i3.sh\)|# \1|' ~/.config/i3/config
+cp /etc/xdg/autostart/firewall-applet.desktop ~/.config/autostart/
+echo Hidden=true >> ~/.config/autostart/firewall-applet.desktop
 rm ~/xed.dconf
 rm ~/set_once_i3.sh
