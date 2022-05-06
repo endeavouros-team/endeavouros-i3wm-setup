7463fa4a01c9928a454493e9357f7cec24f81414
Johannes Kamprad <archlinux@kamprad.net>
2022-05-06 23:53:25 +0200

    Update set_once_i3.sh
    chmod +x ~/.config/autostart/firewall-applet.desktop

--- set_once_i3.sh
+++ set_once_i3.sh
@@ -4,4 +4,5 @@ chmod -R +x ~/.config/i3/scripts
 sed -i 's|\(exec --no-startup-id ~/set_once_i3.sh\)|# \1|' ~/.config/i3/config
 cp /etc/xdg/autostart/firewall-applet.desktop ~/.config/autostart/
 echo Hidden=true >> ~/.config/autostart/firewall-applet.desktop
+chmod +x ~/.config/autostart/firewall-applet.desktop
 rm ~/xed.dconf ~/set_once_i3.sh 
