71b66b036505b515a6984e1e180e478705181cd0
Johannes Kamprad <archlinux@kamprad.net>
2022-05-08 12:33:41 +0200

    Update set_once_i3.sh
    removed firewall applet autostart stop to add it directly into skel (default)

--- set_once_i3.sh
+++ set_once_i3.sh
@@ -2,7 +2,4 @@
 dbus-launch dconf load / < ~/xed.dconf
 chmod -R +x ~/.config/i3/scripts
 sed -i 's|\(exec --no-startup-id ~/set_once_i3.sh\)|# \1|' ~/.config/i3/config
-cp /etc/xdg/autostart/firewall-applet.desktop ~/.config/autostart/
-echo Hidden=true >> ~/.config/autostart/firewall-applet.desktop
-chmod +x ~/.config/autostart/firewall-applet.desktop
 rm ~/xed.dconf ~/set_once_i3.sh 
