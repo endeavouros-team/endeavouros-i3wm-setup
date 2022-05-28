efcd79f2ae3e57ae27c7e287ea37a0995a569d86
Johannes Kamprad <archlinux@kamprad.net>
2022-05-28 13:24:13 +0200

    Delete set_once_i3.sh
    not needed because we can use the same as used for xfce4 (desktop autostart)

--- set_once_i3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dbus-launch dconf load / < ~/xed.dconf
-chmod -R +x ~/.config/i3/scripts
-sed -i 's|\(exec --no-startup-id ~/set_once_i3.sh\)|# \1|' ~/.config/i3/config
-rm ~/xed.dconf ~/set_once_i3.sh 
